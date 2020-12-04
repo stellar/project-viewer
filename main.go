e97bb8ae47889074e1901e9304cc31d27b2d8703
Isaiah-Turner <42742035+Isaiah-Turner@users.noreply.github.com>
2020-12-03 19:33:24 -0500

    Added volume endpoint

--- main.go
+++ main.go
@@ -5,13 +5,14 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi"
-	"github.com/stellar/project-viewer/projectviewer"
+	"github.com/stellar/project-viewer/backend"
 )
 
 // ServeMux creates Mux to serve api
 func ServeMux() http.Handler {
 	mux := chi.NewMux()
-	mux.Handle("/corridor", projectviewer.CorridorHandler())
+	mux.Handle("/corridor", backend.CorridorHandler())
+	mux.Handle("/volume", backend.VolumeHandler())
 	return mux
 }
 
@@ -30,4 +31,9 @@ Some example URLs:
 Corridors:
 http://localhost:8080/corridor?sourceCode=NGNT&sourceIssuer=GAWODAROMJ33V5YDFY3NPYTHVYQG7MJXVJ2ND3AOGIHYRWINES6ACCPD&destCode=EURT&destIssuer=GAP5LETOV6YIE62YAM56STDANPRDO7ZFDBGSNHJQIYGGKSMOZAHOOS2S
 http://localhost:8080/corridor?sourceCode=CENTUS&sourceIssuer=GAKMVPHBET4T7DPN32ODVSI4AA3YEZX2GHGNNSBGFNRQ6QEVKFO4MNDZ&destCode=USD&destIssuer=GB2O5PBQJDAFCNM2U2DIMVAEI7ISOYL4UJDTLN42JYYXAENKBWY6OBKZ
+
+Volumes:
+http://localhost:8080/volume?code=NGNT&issuer=GAWODAROMJ33V5YDFY3NPYTHVYQG7MJXVJ2ND3AOGIHYRWINES6ACCPD
+http://localhost:8080/volume?code=CENTUS&issuer=GAKMVPHBET4T7DPN32ODVSI4AA3YEZX2GHGNNSBGFNRQ6QEVKFO4MNDZ&isSource=true
+
 */
