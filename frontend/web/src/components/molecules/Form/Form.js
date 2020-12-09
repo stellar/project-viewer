3235d86687d9530167bda91d86e57b4d08eddd30
Isaiah-Turner <42742035+Isaiah-Turner@users.noreply.github.com>
2020-12-08 21:26:39 -0500

    Changed input to dropdown menu

--- frontend/web/src/components/molecules/Form/Form.js
+++ frontend/web/src/components/molecules/Form/Form.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import LabelledInput from '../LabelledInput/LabelledInput';
 import getCorridorInfo from '../../../api/getCorridorInfo'
 import getVolumeInfo from '../../../api/getVolumeInfo';
+import getAssetInfo from '../../../api/getAssetInfo';
 class Form extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      fromCode: "CENTUS",
-      fromIssuer: "GAKMVPHBET4T7DPN32ODVSI4AA3YEZX2GHGNNSBGFNRQ6QEVKFO4MNDZ",
-      toCode: "USD",
-      toIssuer: "GB2O5PBQJDAFCNM2U2DIMVAEI7ISOYL4UJDTLN42JYYXAENKBWY6OBKZ",
+      fromAsset: "",
+      toAsset: "",
+      assets: getAssetInfo(this.props.baseUrl),
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -20,29 +20,35 @@ class Form extends Component {
     const target = event.target;
     const name = target.name;
     const value = target.value
+    console.log(target)
     this.setState({
       [name]: value
     });
-
   }
 
   handleSubmit(event) {
     console.log("Submitting with params: ", this.state)
-    if (this.state.fromCode !== ""  && this.state.fromIssuer !== "" && this.state.toCode !== "" && this.state.toIssuer !== "") {
-      console.log(`Finding volume between ${this.state.fromCode}->${this.state.toCode}`)
+    if (this.state.fromAsset !== ""  && this.state.toAsset !== "") {
+      console.log(`Finding volume between ${this.state.fromAsset.code}->${this.state.toAsset.code}`)
 
-      getCorridorInfo(this.props.baseUrl, this.state.fromCode, this.state.fromIssuer, this.state.toCode, this.state.toIssuer).then(
+      let fromAsset = JSON.parse(this.state.fromAsset)
+      let toAsset = JSON.parse(this.state.toAsset)
+      getCorridorInfo(this.props.baseUrl, fromAsset.code, fromAsset.issuer, toAsset.code, toAsset.issuer).then(
         response => {this.props.handler(response)})
 
-    } else if (this.state.fromCode !== ""  && this.state.fromIssuer !== "" && this.state.toCode === "" && this.state.toIssuer === "") {
-      console.log(`Finding volume from ${this.state.fromCode}`)
+    } else if (this.state.fromAsset !== ""  && this.state.toAsset === "") {
+      let fromAsset = JSON.parse(this.state.fromAsset)
 
-      getVolumeInfo(this.props.baseUrl, this.state.fromCode, this.state.fromIssuer, "true").then(
+      console.log(`Finding volume from ${fromAsset.code}`)
+
+      getVolumeInfo(this.props.baseUrl, fromAsset.code, fromAsset.issuer, "true").then(
         response => {this.props.handler(response)})
-    } else if (this.state.fromCode === ""  && this.state.fromIssuer === "" && this.state.toCode !== "" && this.state.toIssuer !== "") {
-      console.log(`Finding volume to ${this.state.toCode}`)
+    } else if (this.state.fromAsset === ""  && this.state.toAsset !== "") {
+      let toAsset = JSON.parse(this.state.toAsset)
 
-      getVolumeInfo(this.props.baseUrl, this.state.toCode, this.state.toIssuer, "").then(
+      console.log(`Finding volume to ${toAsset.code}`)
+
+      getVolumeInfo(this.props.baseUrl, toAsset.code, toAsset.issuer, "").then(
         response => {this.props.handler(response)})
     } else {
        this.props.handler({"results": "parameters are malformed"})
@@ -54,10 +60,8 @@ class Form extends Component {
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-            <LabelledInput title="From Code" id="fromCode" value={this.state.fromCode} changeHandler={this.handleChange}/>
-            <LabelledInput title="From Issuer" id="fromIssuer" value={this.state.fromIssuer} changeHandler={this.handleChange}/>
-            <LabelledInput title="To Code" id="toCode" value={this.state.toCode} changeHandler={this.handleChange}/>
-            <LabelledInput title="To Issuer" id="toIssuer" value={this.state.toIssuer} changeHandler={this.handleChange}/>
+            <LabelledInput title="From" id="fromAsset" value={this.state.fromAsset} changeHandler={this.handleChange} assets={this.state.assets}/>
+            <LabelledInput title="To" id="toAsset" value={this.state.toAsset} changeHandler={this.handleChange} assets={this.state.assets}/>
             <input type="submit" value="Submit"></input>
       </form>
     );
