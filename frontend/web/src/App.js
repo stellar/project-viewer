7b53d94053dd0e56d2f39b0f66e65805136547b2
Iveta <quietbits@gmail.com>
2020-12-09 16:38:20 -0500

    Frontend: Add configs for web

--- frontend/web/src/App.js
+++ frontend/web/src/App.js
@@ -1,30 +1,32 @@
-import logo from './logo.svg';
-import React, { Component } from 'react';
-import './App.css';
-import Form from './components/molecules/Form/Form';
-import DataTable from './components/molecules/DataTable/DataTable';
+import React, { Component } from "react";
+import "./App.css";
+import Form from "./components/molecules/Form/Form";
+import DataTable from "./components/molecules/DataTable/DataTable";
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      data: ""
-    }
-    this.queryResultHandler = this.queryResultHandler.bind(this)
+      data: "",
+    };
+    this.queryResultHandler = this.queryResultHandler.bind(this);
   }
 
   queryResultHandler(response) {
     this.setState({
-      data: JSON.stringify(response.results)
-    })
-    console.log("From parent, now our state is", this.state)
+      data: JSON.stringify(response.results),
+    });
+    console.log("From parent, now our state is", this.state);
   }
 
   render() {
     return (
       <div className="App">
-        <Form handler={this.queryResultHandler} baseUrl="http://localhost:8080"/>
-        <DataTable data={this.state.data}/>
+        <Form
+          handler={this.queryResultHandler}
+          baseUrl="http://localhost:8080"
+        />
+        <DataTable data={this.state.data} />
       </div>
     );
   }
